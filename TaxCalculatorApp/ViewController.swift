c3e50a9f2cc11fe06cd43bfa1e9605cbd4bf196e
YukiYouda <youda0518@yahoo.co.jp>
2021-11-09 13:09:28 +0900

    Initial Commit

---
+++ TaxCalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaxCalculatorApp
+//
+//  Created by YUKI YOUDA on 2021/11/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
